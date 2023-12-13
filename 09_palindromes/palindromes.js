af5feaf2788113492bc6a3ff9485bddf14060dc9
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2023-12-13 13:36:21 +0330

    Start work on 09_palindromes. 2 tests are failing
    Fails on
    (palindromes('Animal loots foliated detail of stool lamina.'))
    (palindromes('r3ace3car'))

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // take in the string, remove all commas and spaces
+    // reverse it
+    // compare if the reversed string and original string are the same
+    let stringLowerCase = string.toLowerCase();
+    console.log(stringLowerCase);
+    let stringReverse = stringLowerCase.split(/,|\s|!|./).reverse().join('');
+    console.log(stringReverse);
+    return stringLowerCase === stringReverse ? true : false;
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
