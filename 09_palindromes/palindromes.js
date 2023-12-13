4278fa99db4679689c9e744d46e5fa270f1b4221
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2023-12-14 00:54:56 +0330

    fin 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,14 +2,10 @@ const palindromes = function (string) {
     // take in the string, remove all commas and spaces
     // reverse it
     // compare if the reversed string and original string are the same
-    let stringLowerCase = string.toLowerCase();
-    console.log(stringLowerCase);
-    let stringReverse = stringLowerCase.split(/,|\s|!|./).reverse().join('');
-    console.log(stringReverse);
-    return stringLowerCase === stringReverse ? true : false;
+    let stringClean = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let stringReverse = stringClean.split('').reverse().join('');
+    return stringClean === stringReverse;
 };
-
-console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
