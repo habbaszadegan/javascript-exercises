321cba67e143ef974d693a20cba3618f8f102b31
Hossein Abbaszadegan <haabaszadegan@gmail.com>
2023-12-16 11:08:59 +0330

    cleaned 10_fibonacci code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -23,23 +23,3 @@ const fibonacci = function(x) {
 
 // Do not edit below this line
 module.exports = fibonacci;
-
-
-
-// # Exercise 10 - Fibonacci
-
-
-// const totalYears = inventors.reduce((total, inventor) => {
-//     return total + (inventor.passed - inventor.year);
-//   }, 0);
-
-// Create a function that returns a specific member of the Fibonacci sequence:
-
-// > A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
-// > In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
-// > To learn more about Fibonacci sequences, go to: https://en.wikipedia.org/wiki/Fibonacci_sequence
-
-// ```javascript
-// fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
-// fibonacci(6); // returns 8
-// ```
