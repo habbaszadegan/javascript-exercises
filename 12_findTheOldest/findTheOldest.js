ee5cbe2d561549c44b695f5e05c82f24c823ee06
Hossein Abbaszadegan <haabaszadegan@gmail.com>
2023-12-16 20:00:17 +0330

    Working on 12_findTheOldest
    Current solution "Not Working": update objects with age property. create
    a compareAge variable that gets updated every time the age is higher.
    return the name with the highest age.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,53 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    // loop over the array of objects
+    // calculate the person with the greatest age
+    // update the array of objects with current age
+    // run an if statement to see which age is higher
+    for (let i = 0; i < array.length; i++) {
+        array[i].age = array[i].yearOfDeath - array[i].yearOfBirth;
+    }
+    let ageCompare = 0;
+    for (let j = 0; j < array.length; j++) {
+        if (array[j].age > ageCompare) {
+            ageCompare = array[j].age;
+        }
+    }
+    for (let k = 0; k < array.length; k++) {
+        if (array[k].age === ageCompare) {
+            return array[k].name;
+        }
+    }
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+// finds the person with the greatest age
+//    if (someone is living)
+//    if (the oldest is living)
+
+// let name
+//      yearOfBirth
+//      yearOfDeath
+
+// Given an array of objects representing people with a birth and death year, return the oldest person.
